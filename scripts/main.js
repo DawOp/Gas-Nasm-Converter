796e3be00924c5b88a0e48fef3fd82e9f37dca8f
DawOp <dawidopiola2@gmail.com>
2022-05-16 12:45:07 +0200

    add three code editors

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,20 @@ import "../styles/main.css";
 let nasm_editor = CodeMirror.fromTextArea(document.getElementById('gas-editor'), { 
     mode: "gas",
     theme: "dracula",
-    tabSize: 4
+    tabSize: 4,
+    lineNumbers: true
 });
 
 let gas_editor = CodeMirror.fromTextArea(document.getElementById('nasm-editor'), { 
     mode: "gas",
     theme: "dracula",
-    tabSize: 4
+    tabSize: 4,
+    lineNumbers: true
 });
 
 let output_editor = CodeMirror.fromTextArea(document.getElementById('output-editor'), { 
     mode: "gas",
     theme: "dracula",
-    tabSize: 4
+    tabSize: 4,
+    lineNumbers: true
 });
