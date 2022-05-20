7e536c17ae1e62450be3d940b7ae3a8e591ff019
DawOp <dawidopiola2@gmail.com>
2022-05-20 17:21:29 +0200

    add loader for conversion

--- scripts/editors-display.js
+++ scripts/editors-display.js
@@ -6,17 +6,18 @@ function changeEditor() {
 }
 
 input_editor.on("change", function() {
-    convert_timer = 3;
+    document.getElementById("conversion-loader").style.visibility = "visible";
+    convert_timer = 2;
 });
 
 function convert_listener() {
     if (convert_timer == 0) {
-        output_editor.setValue(input_editor.getValue());
-        // output_editor.setValue("<Compilation Error>");
+        output_editor.setValue("<Compilation Error>");
+        document.getElementById("conversion-loader").style.visibility = "hidden";
     }
     else {
         convert_timer--;
     }
 }
 
-setInterval(convert_listener,500); // 5
+setInterval(convert_listener,1000); // 5
